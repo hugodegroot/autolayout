d1c938acd918989f81159189253837479dd8fcd2
Hugo de Groot <hugodegroot97@gmail.com>
2019-06-18 06:19:42 +0200

    Initial Commit

---
+++ autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by Hugo de Groot on 18/06/2019.
+//  Copyright © 2019 Hugo de Groot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
